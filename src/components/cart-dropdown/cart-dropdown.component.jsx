34448f234d22ba72945b50f1bf02aa3ddfd23252
Iwie Rodriguez <luigiramirezrodriguez@gmail.com>
2021-04-29 15:34:09 +0800

    updateing our application with reslect, replace cart selector with momoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import CustomButtom from '../custom-buttom/custom-buttom.components';
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
 import './cart-dropdown.styles.scss';
 
 const CartDropDown = ( { cartItems } ) => (
@@ -16,8 +18,8 @@ const CartDropDown = ( { cartItems } ) => (
     </div>
 )
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropDown);
