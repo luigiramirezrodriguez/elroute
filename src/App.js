06712cdb4b7e16ca317ca7b2e791024349ff88f4
Iwie Rodriguez <luigiramirezrodriguez@gmail.com>
2021-04-29 17:21:35 +0800

    added checkout page, checkout item component, and updated cart to use selectors from reselect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.components';
 import ShopPage from './pages/shop/shop.components';
 import SignInandSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.components'; 
+import CheckoutPage from './pages/checkout/checkout.components';
 import Header from './components/header/header.components';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'; 
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component { 
   
@@ -52,6 +55,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          
           <Route 
             exact 
             path='/signin' 
@@ -71,8 +76,8 @@ class App extends React.Component {
 }
 
 
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispachToProps = dispatch => ({
